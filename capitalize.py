4dfd13f3bc321cca7c3935c961224eb5ecdc7401
Rabeeh K T <35598587+rabeehkt@users.noreply.github.com>
2020-07-29 20:48:34 +0530

    added question

--- capitalize.py
+++ capitalize.py
@@ -1,3 +1,17 @@
+"""
+Question:
+
+Given a paragraph count the frequency of vowels and consonants in each word and 
+capitalize the vowels/consonants in each word whose frequency is the highest.
+If the frequency of vowels and consonants are equal then capitalize the whole word.
+
+Sample Input and Output
+
+Input : Hey! How are you? I hope you are fine.
+Output: HeY! HoW ArE yOU? I HOPE yOU ArE FINE.
+"""
+
+
 para = input("Enter the string: ")
 vowels = 'aeiouAEIOU'
 consonants = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
